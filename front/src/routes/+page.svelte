848c87a1ba5019f4c3b134504cd826611c79ad80
sebasion9 <kansebakk@gmail.com>
2024-11-30 15:44:01 +0100

    add frontend proxy

--- front/src/routes/+page.svelte
+++ front/src/routes/+page.svelte
@@ -1,7 +1,35 @@
-<script>
+<script lang="ts">
     import Answer from './answer.svelte';
     import Question from './question.svelte';
     import Button from './finishButton.svelte';
+    import { onMount } from 'svelte';
+
+    type QuestionStage = "ASKED" | "ANSWERERD" | "RATED";
+    interface QuestionModel {
+           stage: QuestionStage
+           //
+           // Stage ASKED
+           question: string
+           options: string[]
+           shortOptions: string[]
+
+           // Stage ANSWERED
+           answer?: string
+
+           // Stage RATED
+           result?: string
+           accuracy?: number
+
+    }
+    let question1 : QuestionModel;
+
+    onMount(async () => {
+        let res = await fetch("api/questions?session=1");
+        let body = res.body;
+        console.log(body);
+
+    });
+
 </script>
 
 <style lang="scss">

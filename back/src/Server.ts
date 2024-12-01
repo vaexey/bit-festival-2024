64c121712b91f1a12a562ed04a33531fe3473c1a
vaexey <vaexey@gmail.com>
2024-12-01 09:27:05 +0100

    add error handlers

--- back/src/Server.ts
+++ back/src/Server.ts
@@ -34,53 +34,64 @@ export class Server
         })
 
         this.app.get("/api/newSession", async (req: SesRes, res) => {
-            const id = (++this.sessionIncr)+""
-
-            const newSession = new HobbySession(
-                id,
-                this.llm
-            )
-
-            this.sessions.push(newSession)
-
-            await newSession.loadFirstQuestion()
-
-            // res.redirect(`/api/questions?session=${id}`)
-            res.send(id)
-            res.end()
+            try {
+                const id = (++this.sessionIncr)+""
+    
+                const newSession = new HobbySession(
+                    id,
+                    this.llm
+                )
+    
+                this.sessions.push(newSession)
+    
+                await newSession.loadFirstQuestion()
+    
+                res.send(id)
+                res.end()
+            } catch (error) {
+                res.status(500).send(error)
+            }
         })
 
         this.app.get("/api/questions", async (req: SesRes, res) => {
-            if(!req.session)
-            {
-                res.sendStatus(401);
-                return
+            try{
+                if(!req.session)
+                {
+                    res.sendStatus(401);
+                    return
+                }
+
+                res.send(JSON.stringify(req.session.questions, null, 4))
+
+                res.end();
+            } catch (error) {
+                res.status(500).send(error)
             }
-
-            res.send(JSON.stringify(req.session.questions, null, 4))
-
-            res.end();
         })
 
         this.app.get("/api/answer", async (req: SesRes, res) => {
-            if(!req.session)
-            {
-                res.sendStatus(401);
-                return;
+            try{
+                if(!req.session)
+                {
+                    res.sendStatus(401);
+                    return;
+                }
+
+                const answer = req.query['content']
+
+                if(!answer)
+                {
+                    res.sendStatus(400)
+                    return;
+                }
+
+                await req.session.answer(answer + "")
+
+                res.sendStatus(200)
+                return
+            } catch (error) {
+                res.status(500).send(error)
             }
-
-            const answer = req.query['content']
-
-            if(!answer)
-            {
-                res.sendStatus(400)
-                return;
-            }
-
-            await req.session.answer(answer + "")
-
-            res.sendStatus(200)
-            return
         })
 
         this.app.get("/api/results", async (req: SesRes, res) => {
